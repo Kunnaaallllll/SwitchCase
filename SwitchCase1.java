46d6368f693a25071556c8a523ffb80cff279302
Kunnaaallllll <869796kunalchaudhary@gmail.com>
2025-01-31 14:55:03 +0530

    new commited

--- SwitchCase1.java
+++ SwitchCase1.java
@@ -1,26 +1,16 @@
-package capgemini.SwitchCase;
-
-import java.util.Scanner;
+import java.util.*;
 public class SwitchCase1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-<<<<<<< HEAD
         System.out.print("Enter first number: ");
         double num1 = sc.nextDouble();
-// hwllo 
-=======
-       
 
->>>>>>> f03ad4a86139af97ff72a834c70e292916f4efb8
         System.out.print("Enter second number: ");
         double num2 = sc.nextDouble();
 
-        // hii how are yoyr?
         System.out.print("Enter an operator (+, -, *, /): ");
-        char operator = sc.next().charAt(0);
-
+        double operator = sc.next().charAt(0);
         double result;
 
         switch (operator) {
